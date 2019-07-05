07bb5b4141e3e7134102fcfced8f9099c8fbae04
Johnathan James <haxwell@gmail.com>
2019-07-05 13:05:22 -0600

    Basic functionality to allow keeping track of a grade for each time a question is presented to a candidate is in place.

---
+++ src/main/java/org/haxwell/dtim/techprofile/entities/CandidateQuestionGrade.java
@@ -0,0 +1,64 @@
+package org.haxwell.dtim.techprofile.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
+@Entity
+@IdClass(CandidateQuestionGradeId.class)
+public class CandidateQuestionGrade {
+
+	@Id
+	private Long candidateId;
+	
+	public Long getCandidateId() {
+		return this.candidateId;
+	}
+	
+	public void setCandidateId(Long candidateId) {
+		this.candidateId = candidateId;
+	}
+	
+	@Id
+	private Long sessionId;
+	
+	public Long getSessionId() {
+		return this.sessionId;
+	}
+	
+	public void setSessionId(Long sId) {
+		this.sessionId = sId;
+	}
+	
+	@Id
+	private Long questionId;
+	
+	public Long getQuestionId() {
+		return this.questionId;
+	}
+	
+	public void setQuestionId(Long qId) {
+		this.questionId = qId;
+	}
+	
+	private Long grade;
+	
+    public Long getGrade() {
+		return grade;
+	}
+
+	public void setGrade(Long grade) {
+		this.grade = grade;
+	}
+
+	public CandidateQuestionGrade(Long candidateId, Long sessionId, Long questionId, Long grade) {
+		this.candidateId = candidateId;
+		this.sessionId = sessionId;
+		this.questionId = questionId;
+		this.grade = grade;
+    }
+
+	public CandidateQuestionGrade() {
+
+    }
+}
---
+++ src/main/java/org/haxwell/dtim/techprofile/entities/CandidateQuestionGradeId.java
@@ -0,0 +1,10 @@
+package org.haxwell.dtim.techprofile.entities;
+
+import java.io.Serializable;
+
+public class CandidateQuestionGradeId implements Serializable {
+
+	private Long candidateId;
+	private Long sessionId;
+	private Long questionId;
+}
