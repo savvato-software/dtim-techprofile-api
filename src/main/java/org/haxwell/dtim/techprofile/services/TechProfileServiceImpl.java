26f76acd65fc43c53b900b14265f42006f2d5668
Johnathan James <haxwell@gmail.com>
2023-04-19 23:21:32 -0600

    Minor, imports

--- src/main/java/org/haxwell/dtim/techprofile/services/TechProfileServiceImpl.java
+++ src/main/java/org/haxwell/dtim/techprofile/services/TechProfileServiceImpl.java
@@ -1,24 +1,8 @@
 package org.haxwell.dtim.techprofile.services;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.Function;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.haxwell.dtim.techprofile.entities.Question;
-import org.haxwell.dtim.techprofile.entities.TechProfile;
-import org.haxwell.dtim.techprofile.entities.TechProfileLineItem;
-import org.haxwell.dtim.techprofile.entities.TechProfileTopic;
 import org.haxwell.dtim.techprofile.entities.UserTechProfileLineItemScore;
 import org.haxwell.dtim.techprofile.repositories.TechProfileLineItemRepository;
 import org.haxwell.dtim.techprofile.repositories.TechProfileRepository;
@@ -26,7 +10,15 @@ import org.haxwell.dtim.techprofile.repositories.TechProfileTopicRepository;
 import org.haxwell.dtim.techprofile.repositories.UserTechProfileLineItemScoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
 
 @Service
 public class TechProfileServiceImpl implements TechProfileService {
