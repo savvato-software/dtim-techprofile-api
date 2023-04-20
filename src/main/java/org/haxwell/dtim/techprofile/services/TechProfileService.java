0f2735fccaf901889ab01539a0600e4109320eea
Johnathan James <haxwell@gmail.com>
2023-04-19 23:20:06 -0600

    Removed some methods from TechProfileService which are now found in skills-matrix-api

--- src/main/java/org/haxwell/dtim/techprofile/services/TechProfileService.java
+++ src/main/java/org/haxwell/dtim/techprofile/services/TechProfileService.java
@@ -10,17 +10,6 @@ import org.haxwell.dtim.techprofile.entities.UserTechProfileLineItemScore;
 
 public interface TechProfileService {
 
-	public TechProfile get(Long id);
-
-	public TechProfileTopic addTopic(String topicName);
-	public TechProfileTopic updateTopic(Long topicId, String name);
-
-	public TechProfileLineItem addLineItem(Long topicId, String lineItemName, String l0desc, String l1desc, String l2desc, String l3desc);
-	public Optional<TechProfileLineItem> getLineItem(Long lineItemId);	
-	public TechProfileLineItem updateLineItem(Long lineItemId, String lineItemName, String l0desc, String l1desc, String l2desc, String l3desc);
-	
-	public boolean updateSequencesRelatedToATopicAndItsLineItems(long[] arr);
-	
 	public List<UserTechProfileLineItemScore> getUserIdScores(Long userId);
 	
 	public List getQuestionCountsPerCell();
