f5bedfea808bb1e6055877902ebbabfea6c28d10
Johnathan James <haxwell@gmail.com>
2023-04-19 23:27:43 -0600

    Moved some code dealing with line items to skills-matrix-api, removed from this repo.

--- src/main/java/org/haxwell/dtim/techprofile/services/TechProfileTopicService.java
+++ src/main/java/org/haxwell/dtim/techprofile/services/TechProfileTopicService.java
@@ -4,6 +4,4 @@ import org.haxwell.dtim.techprofile.entities.Question;
 
 public interface TechProfileTopicService {
 	Iterable<Question> getQuestionsByTopic(Long topicId);
-	boolean addExistingLineItemAsChild(Long topicId, Long existingLineItemId);
-	boolean removeLineItemAsChild(Long topicId, Long existingLineItemId);
 }
